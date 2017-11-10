4b64316b22a0bf55dd7788223a736e9d79470c18
German Rodriguez <german.rodriguez@correo.ucu.edu.uy>
2017-11-10 20:30:21 -0300

    showBoard, fix Piece data

--- Contingency.hs
+++ Contingency.hs
@@ -15,7 +15,17 @@ data ContingencyPlayer = PlayerTrue | PlayerFalse deriving (Eq, Show, Enum)
 data ContingencyGame   = Board [Piece] -- 7*7 = 49
 data ContingencyAction = Move Operador Bool (Int,Int)
 
-data Piece = Empty | Hidden Bool | True_ | False_ | Op Operador Bool
+data Piece = Empty | Hidden Bool | True_ | False_ | OpAnd Bool
+             | OpOr Bool | OpIff Bool | OpXor Bool
+instance Show Piece where
+  show Empty = "."
+  show (Hidden _) = "0"
+  show True_ = "t"
+  show False_ = "f"
+  show (OpAnd _) = "&"
+  show (OpOr _) = "|"
+  show (OpIff _) = "i"
+  show (OpXor _) = "x"
 
 type Operador = (Bool -> Bool -> Bool)
 iff, xor :: Operador
@@ -26,12 +36,12 @@ beginning :: IO ContingencyGame
 beginning = error "beginning has not been implemented!" --TODO
 
 emptyBoard :: ContingencyGame
-emptyBoard = Board (replicate 74 Empty)
+emptyBoard = Board (replicate 49 Empty)
 
 activePlayer :: ContingencyGame -> Maybe ContingencyPlayer
 activePlayer g@(Board b)
   | isFinished g = Nothing
-  | otherwise = if even $ length [x | Op x _ <- b] then (Just PlayerTrue) else (Just PlayerFalse)
+  | otherwise = if even (countOp b) then (Just PlayerTrue) else (Just PlayerFalse)
 
 actions :: ContingencyGame -> ContingencyPlayer -> [ContingencyAction]
 actions _ _ = error "actions has not been implemented!" --TODO
@@ -40,13 +50,13 @@ nextState :: ContingencyGame -> ContingencyPlayer -> ContingencyAction -> IO Con
 nextState _ _ _ = error "nextState has not been implemented!" --TODO
 
 isFinished :: ContingencyGame -> Bool
-isFinished (Board b) = length [x | Op x _ <- b] == 30
+isFinished (Board b) = countOp b == 30
 
 score :: ContingencyGame -> ContingencyPlayer -> Int
 score _ _ = error "score has not been implemented!" --TODO
 
 showBoard :: ContingencyGame -> String
-showBoard _ = error "showBoard has not been implemented!" --TODO
+showBoard (Board b) = unlines $ map concat $ chunksOf 7 (map show b)
 
 showAction :: ContingencyAction -> String
 showAction _ = error "showAction has not been implemented!" --TODO
@@ -80,3 +90,25 @@ runOnConsole :: IO (Int, Int)
 runOnConsole = do
   board <- beginning
   runMatch (PlayerTrue, PlayerFalse) board
+
+-- Auxiliar ---------------------------------------------------------------------------------------
+{-chunksOf :: Int -> [e] -> [[e]]
+chunksOf i ls = map (take i) (build (splitter ls)) where
+  splitter :: [e] -> ([e] -> a -> a) -> a -> a
+  splitter [] _ n = n
+  splitter l c n  = l `c` splitter (drop i l) c n
+
+build :: ((a -> [a] -> [a]) -> [a] -> [a]) -> [a]
+build g = g (:) []
+-}
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf n xs = map (map snd) $ groupBy groupFunction (zip [0..] xs)
+                where groupFunction (i1,_) (i2,_) = (div i1 n) == (div i2 n)
+
+countOp :: [Piece] -> Int
+countOp b = sum $ map fun b
+                    where fun Empty = 0
+                          fun (Hidden _) = 0
+                          fun True_ = 0
+                          fun False_ = 0
+                          fun _ = 1
